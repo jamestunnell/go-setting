62bafa8d63e821474b1e7a1ac74697f8687d54ea
James Tunnell <jamestunnell@gmail.com>
2020-12-15 13:42:20 -0800

    Add convenience functions for new options

--- option/option.go
+++ option/option.go
@@ -15,7 +15,47 @@ func New(typ Type, param interface{}) *Option {
 	return &Option{Type: typ, Param: param}
 }
 
-// String a string representation of the option with the form <type>(<param>)
+// String a string representation of the option with the form <type>:"<param>"
 func (o *Option) String() string {
-	return fmt.Sprintf("%s(%v)", o.Type.String(), o.Param)
+	return fmt.Sprintf("%s:\"%v\"", o.Type.String(), o.Param)
+}
+
+// NewDefault makes an option that specifies default value
+func NewDefault(param interface{}) *Option {
+	return &Option{Type: Default, Param: param}
+}
+
+// NewMinLen makes an option that specifies min string/slice length
+func NewMinLen(param interface{}) *Option {
+	return &Option{Type: MinLen, Param: param}
+}
+
+// NewMaxLen makes an option that specifies max string/slice length
+func NewMaxLen(param interface{}) *Option {
+	return &Option{Type: MaxLen, Param: param}
+}
+
+// NewGreater makes an option that specifies a minimum (non-inclusive) value
+func NewGreater(param interface{}) *Option {
+	return &Option{Type: Greater, Param: param}
+}
+
+// NewGreaterEqual makes an option that specifies a minimum (inclusive) value
+func NewGreaterEqual(param interface{}) *Option {
+	return &Option{Type: GreaterEqual, Param: param}
+}
+
+// NewLess makes an option that specifies a maximum (non-inclusive) value
+func NewLess(param interface{}) *Option {
+	return &Option{Type: Less, Param: param}
+}
+
+// NewLessEqual makes an option that specifies a maximum (inclusive) value
+func NewLessEqual(param interface{}) *Option {
+	return &Option{Type: LessEqual, Param: param}
+}
+
+// NewOneOf makes an option that specifies an enumerated value
+func NewOneOf(param interface{}) *Option {
+	return &Option{Type: OneOf, Param: param}
 }
